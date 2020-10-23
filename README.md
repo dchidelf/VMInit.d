20a53adce14b4187da6ad69e20df3f9d404839e3
Chad Fox <dchidelf@briareus.gigapogo.com>
2020-10-22 23:36:31 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # VMInit.d
 Init script to start and keep vmware workstation VMs running
+
+# Setup
+On my system I store my Virtual Machines in D:\VirtualMachines.  In that directory I also created a folder named init.d.
+For each of the vms I want to start/stop/suspend with the script I create shortcuts to the VM directory like:
+```
+S10.myfirstVM -> D:\VirtualMachines\myfirstVM
+s20.anotherVM -> D:\VirtualMachines\anotherVM
+S30.yetanother -> D:\VirtualMachines\yetanother
+K10.myfirstVM -> D:\VirtualMachines\myfirstVM
+K20.anotherVM -> D:\VirtualMachines\anotherVM
+K30.yetanother -> D:\VirtualMachines\yetanother
+```
+
+Then in the Windows Task Scheduler I setup my script to run with -start on startup and on event Security/4647.
+This keeps the VMs running when I log out, or restart the server.
+
+# Options
+- start : starts the VMs listed with S## in order of the numbers.
+- stop: stops the VMs listed with K## in *reverse* order of the numbers.
+- suspend: suspends the VMs listed with K## in *reverse* order of the numbers.
+
+NOTE: The K## entries work in reverse from what you would expect from normal init.d sort of behavior.  I did this because in my case I just want the servers to start in reverse order from how they started.  It was easier to just duplicate the ordering from the S## links and have the script process them in reverse.
+
+
---
+++ VMInit.ps1
@@ -0,0 +1,65 @@
+﻿param (
+   [switch] $start,
+   [switch] $stop,
+   [switch] $suspend,
+   [string] $delay = 0
+)
+
+
+
+
+function Start-VMs {
+   $VMDELAY = $args[0]
+   $sh = New-Object -ComObject WScript.Shell
+   Get-ChildItem -Path D:\VirtualMachines\init.d\* -Include S* | Sort-Object -Property Name | ForEach-Object {
+    
+    $VMPATH = $sh.CreateShortcut($_.FullName).TargetPath
+    $VMXFILE = Get-ChildItem -Path $VMPATH\* -Include *.vmx | Select-Object -First 1 | % { $_.FullName }
+   
+    Write-Host "Starting $VMPATH"
+
+    vmrun -T ws start "$VMXFILE" nogui
+
+    if ($VMDELAY -gt 0) {
+       Start-Sleep -s $VMDELAY
+    }
+
+   }
+}
+
+function Stop-VMs {
+   $VMDELAY = $args[0]
+   $SUSPEND = $args[1]
+   $sh = New-Object -ComObject WScript.Shell
+   Get-ChildItem -Path D:\VirtualMachines\init.d\* -Include K* | Sort-Object -Descending -Property Name | ForEach-Object {
+    
+    $VMPATH = $sh.CreateShortcut($_.FullName).TargetPath
+    $VMXFILE = Get-ChildItem -Path $VMPATH\* -Include *.vmx | Select-Object -First 1 | % { $_.FullName }
+   
+    
+
+    if ($SUSPEND) {
+       Write-Host "Suspending $VMPATH"
+       vmrun -T ws suspend "$VMXFILE" 
+    } else {
+       Write-Host "Stopping $VMPATH"
+       vmrun -T ws stop "$VMXFILE" 
+    }
+
+    if ($VMDELAY -gt 0) {
+       Start-Sleep -s $VMDELAY
+    }
+
+   }
+}
+
+if ($start) {
+  Start-VMs $delay
+} elseif ($stop) {
+  Stop-VMs $delay 0
+} elseif ($suspend) {
+  Stop-VMs $delay 1
+} else {
+   Write-Host "Specify either -start or -stop"
+   exit
+}
